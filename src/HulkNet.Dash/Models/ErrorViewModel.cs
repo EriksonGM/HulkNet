1ea58137b01d7392599ac80b6f77dc88c5e85039
Erikson Melgarejo <eriksonlove@gmail.com>
2022-03-12 20:07:24 +0100

    Add project files.

---
+++ src/HulkNet.Dash/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HulkNet.Dash.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
